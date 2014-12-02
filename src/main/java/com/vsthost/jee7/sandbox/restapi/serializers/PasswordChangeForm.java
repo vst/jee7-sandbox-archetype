17faef8161292f3dc0805b97c9809fc78c2e74f0
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2014-12-02 18:09:25 +0100

    Documentation added for password form

--- src/main/java/com/vsthost/jee7/sandbox/restapi/serializers/PasswordChangeForm.java
+++ src/main/java/com/vsthost/jee7/sandbox/restapi/serializers/PasswordChangeForm.java
@@ -1,13 +1,45 @@
+/*
+ * Copyright (c) 2013-2014 Vehbi Sinan Tunalioglu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.vsthost.jee7.sandbox.restapi.serializers;
 
 /**
- * Created by vst on 2/12/14.
+ * Provides a password change form.
+ *
+ * @author Vehbi Sinan Tunalioglu
  */
 public class PasswordChangeForm {
+    /**
+     * Defines the old password of the user account.
+     */
     private String oldPassword;
+
+    /**
+     * Defines the new password of the user account.
+     */
     private String newPassword;
+
+    /**
+     * Defines the confirmation for the new password of the user account.
+     */
     private String newPasswordConfirmation;
 
+    /**
+     * Default empty constructor.
+     */
     public PasswordChangeForm () {}
 
     public String getOldPassword() {
@@ -34,6 +66,11 @@ public class PasswordChangeForm {
         this.newPasswordConfirmation = newPasswordConfirmation;
     }
 
+    /**
+     * Checks if the supplied form information is valid.
+     *
+     * @return Boolean indicating if the supplied form information is valid.
+     */
     public boolean isValid () {
         return newPassword.equals(newPasswordConfirmation);
     }
